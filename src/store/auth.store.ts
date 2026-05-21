b6ae6e840e2601c38280465f83152e518231eca4
robsonjerlley <robsonjerlley.work@gmail.com>
2026-05-21 15:16:11 -0300

    auth.Store.ts criado com Zustand

---
+++ src/store/auth.store.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import type { User } from '@/types/models'
+
+interface AuthState {
+    user: User | null
+    token: string | null
+    login: (user: User, token: string) => void
+    logout: () => void
+    isAuthenticated: () => boolean
+}
+
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set, get) =>({
+            user: null,
+            token: null,
+        
+            login: (user, token) =>{
+                localStorage.setItem('token', token)
+                set({user, token})
+            },
+
+            logout: () => {
+                localStorage.removeItem('token')
+                set({user: null, token: null})
+            },
+
+            isAuthenticated: () => get().token !== null,
+        }), 
+        {name: 'auth'},   
+    ),
+)
