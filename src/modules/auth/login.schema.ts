dfbc5e4c95b3daa56db7ddb2a33d337fe5def68c
robsonjerlley <robsonjerlley.work@gmail.com>
2026-05-22 17:39:19 -0300

    tela criada porem com bug, correção necessária

---
+++ src/modules/auth/login.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const loginSchema = z.object({
+  username: z.string().min(1, 'Usuário obrigatório'),
+  password: z.string().min(1, 'Senha obrigatória'),
+})
+
+export type LoginFormData = z.infer<typeof loginSchema>
